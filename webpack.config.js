10eea14d67d0b2ac32cbb727da09d1270d6c2756
Filip Huhta <filip.huhta@consid.se>
2023-02-03 10:46:12 +0100

    added content security policy

--- webpack.config.js
+++ webpack.config.js
@@ -43,13 +43,30 @@ module.exports = {
       {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
-    }
+      }
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html",
       favicon: "assets/images/favicon.ico",
+      headers: {
+        "referrer": "no-referrer"
+      },
+      meta: {
+        'Content-Security-Policy': {
+          'base-uri': "'self'",
+          'default-src': "'self'",
+          'frame-src': "'none'",
+          'img-src': ["'self'", 'data:'],
+          'media-src': "'none'",
+          'object-src': "'none'",
+          'script-src': ["'self'"],
+          'font-src': ["'self'", "https://fonts.gstatic.com"],
+          'style-src': ["'self'", "https://fonts.googleapis.com"],
+        }
+      }
     }),
+
   ],
 };
